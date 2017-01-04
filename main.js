b11a06c74c1ceb5ce7257e249cb0bbbc784fffa9
lucymonie <lucy.monie.hall@gmail.com>
2017-01-04 14:14:24 +0000

    adds month two and month three to mini calendar

--- main.js
+++ main.js
@@ -35,18 +35,51 @@ function makeCalendar() {
 
 window.onload = function() {
   let now = new Date();
-  let curr_month = now.getMonth();
   let year = now.getFullYear();
-  let firstDay = new Date(year, curr_month, 1).toString().split(' ')[0];
+  let month_one = now.getMonth();
+  let month_two, month_three;
+  if((month_one + 2) <= 12) {
+    month_two = month_one + 1;
+    month_three = month_one + 2;
+  } else if(month_one === 11) {
+    month_two = 12;
+    month_three = 0;
+  } else if(month_one === 12) {
+    month_two = 0;
+    month_three = 1;
+  }
+  let firstDay = new Date(year, month_one, 1).toString().split(' ')[0];
   let today = Math.abs(now.toString().split(' ')[2].split('')[1]);
     dateObj.now = now;
-    dateObj.curr_month = calendar_months[curr_month];
+    dateObj.curr_month = calendar_months[month_one];
     dateObj.year = year;
     dateObj.firstDay = firstDay;
-    dateObj.numDays = months_days[curr_month];
+    dateObj.numDays = months_days[month_one];
     dateObj.emptyDays = days.indexOf(firstDay);
     dateObj.rows = Math.ceil((dateObj.numDays + dateObj.emptyDays) / 7);
     dateObj.today = today;
     dateObj.htmlStr = `<table><caption class="calendar__head">${dateObj.curr_month} ${dateObj.year}</caption><thead><tr><th class="col">M</th><th class="col">T</th><th class="col">W</th><th class="col">T</th><th class="col">F</th><th class="col">S</th><th class="col">S</th></tr></thead><tbody class="table__body">`
+  makeCalendar();
+    dateObj.curr_month = calendar_months[month_two];
+    firstDay = new Date(year, month_two, 1).toString().split(' ')[0];
+    dateObj.firstDay = firstDay;
+    dateObj.emptyDays = days.indexOf(firstDay);
+    dateObj.numDays = months_days[month_two];
+    dateObj.rows = Math.ceil((dateObj.numDays + dateObj.emptyDays) / 7);
+    dateObj.today = null;
+    if(month_two === 0) year++;
+    dateObj.year = year;
+    dateObj.htmlStr += `<table><caption class="calendar__head">${dateObj.curr_month} ${dateObj.year}</caption><thead><tr><th class="col">M</th><th class="col">T</th><th class="col">W</th><th class="col">T</th><th class="col">F</th><th class="col">S</th><th class="col">S</th></tr></thead><tbody class="table__body">`
+  makeCalendar();
+    dateObj.curr_month = calendar_months[month_three];
+    firstDay = new Date(year, month_three, 1).toString().split(' ')[0];
+    dateObj.firstDay = firstDay;
+    dateObj.emptyDays = days.indexOf(firstDay);
+    dateObj.numDays = months_days[month_two];
+    dateObj.rows = Math.ceil((dateObj.numDays + dateObj.emptyDays) / 7);
+    dateObj.today = null;
+    if(month_two === 0) year++;
+    dateObj.year = year;
+    dateObj.htmlStr += `<table><caption class="calendar__head">${dateObj.curr_month} ${dateObj.year}</caption><thead><tr><th class="col">M</th><th class="col">T</th><th class="col">W</th><th class="col">T</th><th class="col">F</th><th class="col">S</th><th class="col">S</th></tr></thead><tbody class="table__body">`
   makeCalendar();
 }
