0896423655cc6989d4e859dab9ecb90aacd7b27e
lucymonie <lucy.monie.hall@gmail.com>
2017-01-04 21:17:29 +0000

    adjusts code to have only one mini calendar with a scroll back or next function

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const calendar_months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const months_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const back = document.querySelector('.back');
+const next = document.querySelector('.next');
 
 let dateObj = {};
-
 console.log(dateObj);
 
 function makeCalendar() {
-  let calendar = document.querySelector('.calendar');
-  dateObj.htmlStr += `<tr>`;
+  let calendar1 = document.querySelector('.calendar1');
+  let calendar2 = document.querySelector('.calendar2');
+  let calendar3 = document.querySelector('.calendar3');
+  dateObj.htmlStr += `<thead><tr><th class="col">M</th><th class="col">T</th><th class="col">W</th><th class="col">T</th><th class="col">F</th><th class="col">S</th><th class="col">S</th></tr></thead><tbody class="table__body"><tr>`;
   for (i = 0; i < dateObj.emptyDays; i++) {
     dateObj.htmlStr += `<td class="col"></td>`
   }
@@ -29,57 +32,56 @@ function makeCalendar() {
     dateObj.htmlStr += '</tr>';
     dateObj.monStart = j;
   }
-  dateObj.htmlStr += `</tbody></table>`
-  calendar.innerHTML = dateObj.htmlStr;
+  dateObj.htmlStr += `</tbody></table>`;
+  if (dateObj.calendar === 'calendar1') calendar1.innerHTML = dateObj.htmlStr;
+  if (dateObj.calendar === 'calendar2') calendar2.innerHTML = dateObj.htmlStr;
+  if (dateObj.calendar === 'calendar3') calendar3.innerHTML = dateObj.htmlStr;
+}
+
+function buildCalendar() {
+  let firstDay = new Date(dateObj.year, dateObj.month_1, 1).toString().split(' ')[0];
+    dateObj.calendar = 'calendar1';
+    dateObj.firstDay = firstDay;
+    dateObj.numDays = months_days[dateObj.month_1];
+    dateObj.emptyDays = days.indexOf(firstDay);
+    dateObj.rows = Math.ceil((dateObj.numDays + dateObj.emptyDays) / 7);
+    if(dateObj.month_1 !== new Date().getMonth()) dateObj.today = null;
+    dateObj.htmlStr = `<table><caption class="calendar__head">${calendar_months[dateObj.month_1]} ${dateObj.year}</caption>`;
+  makeCalendar();
 }
 
 window.onload = function() {
   let now = new Date();
-  let year = now.getFullYear();
-  let month_one = now.getMonth();
-  let month_two, month_three;
-  if((month_one + 2) <= 12) {
-    month_two = month_one + 1;
-    month_three = month_one + 2;
-  } else if(month_one === 11) {
-    month_two = 12;
-    month_three = 0;
-  } else if(month_one === 12) {
-    month_two = 0;
-    month_three = 1;
-  }
-  let firstDay = new Date(year, month_one, 1).toString().split(' ')[0];
-  let today = Math.abs(now.toString().split(' ')[2].split('')[1]);
     dateObj.now = now;
-    dateObj.curr_month = calendar_months[month_one];
+  let month_one = now.getMonth();
+    dateObj.month_1 = month_one;
+  let year = now.getFullYear();
     dateObj.year = year;
-    dateObj.firstDay = firstDay;
-    dateObj.numDays = months_days[month_one];
-    dateObj.emptyDays = days.indexOf(firstDay);
-    dateObj.rows = Math.ceil((dateObj.numDays + dateObj.emptyDays) / 7);
+  let today = Math.abs((dateObj.now).toString().split(' ')[2].split('')[1]);
     dateObj.today = today;
-    dateObj.htmlStr = `<table><caption class="calendar__head">${dateObj.curr_month} ${dateObj.year}</caption><thead><tr><th class="col">M</th><th class="col">T</th><th class="col">W</th><th class="col">T</th><th class="col">F</th><th class="col">S</th><th class="col">S</th></tr></thead><tbody class="table__body">`
-  makeCalendar();
-    dateObj.curr_month = calendar_months[month_two];
-    firstDay = new Date(year, month_two, 1).toString().split(' ')[0];
-    dateObj.firstDay = firstDay;
-    dateObj.emptyDays = days.indexOf(firstDay);
-    dateObj.numDays = months_days[month_two];
-    dateObj.rows = Math.ceil((dateObj.numDays + dateObj.emptyDays) / 7);
-    dateObj.today = null;
-    if(month_two === 0) year++;
-    dateObj.year = year;
-    dateObj.htmlStr += `<table><caption class="calendar__head">${dateObj.curr_month} ${dateObj.year}</caption><thead><tr><th class="col">M</th><th class="col">T</th><th class="col">W</th><th class="col">T</th><th class="col">F</th><th class="col">S</th><th class="col">S</th></tr></thead><tbody class="table__body">`
-  makeCalendar();
-    dateObj.curr_month = calendar_months[month_three];
-    firstDay = new Date(year, month_three, 1).toString().split(' ')[0];
-    dateObj.firstDay = firstDay;
-    dateObj.emptyDays = days.indexOf(firstDay);
-    dateObj.numDays = months_days[month_two];
-    dateObj.rows = Math.ceil((dateObj.numDays + dateObj.emptyDays) / 7);
-    dateObj.today = null;
-    if(month_two === 0) year++;
-    dateObj.year = year;
-    dateObj.htmlStr += `<table><caption class="calendar__head">${dateObj.curr_month} ${dateObj.year}</caption><thead><tr><th class="col">M</th><th class="col">T</th><th class="col">W</th><th class="col">T</th><th class="col">F</th><th class="col">S</th><th class="col">S</th></tr></thead><tbody class="table__body">`
-  makeCalendar();
+  buildCalendar(month_one);
 }
+
+back.addEventListener('click', (e) => {
+  let month_one, year;
+  if(dateObj.month_1 > 0) month_one = dateObj.month_1 - 1;
+  else {
+    month_one = 11;
+    year = dateObj.year - 1;
+    dateObj.year = year;
+  }
+  dateObj.month_1 = month_one;
+  buildCalendar(month_one);
+});
+
+next.addEventListener('click', (e) => {
+  let month_one, year;
+  if(dateObj.month_1 < 11) month_one = dateObj.month_1 + 1;
+  else {
+    month_one = 0;
+    year = dateObj.year + 1;
+    dateObj.year = year;
+  }
+  dateObj.month_1 = month_one;
+  buildCalendar(month_one);
+});
