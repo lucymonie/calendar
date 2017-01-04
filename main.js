e9da6dea59ef3523137f93ffd3950c60811447f7
lucymonie <lucy.monie.hall@gmail.com>
2017-01-04 12:14:27 +0000

    alters code so whole of calendar is constructed in js

--- main.js
+++ main.js
@@ -6,55 +6,47 @@ let dateObj = {};
 
 console.log(dateObj);
 
-function makeCalendarHeader() {
-  let calHead = document.querySelector('.calendar__head');
-    calHead.textContent = `${dateObj.month} ${dateObj.year}`;
-  createFirstRow();
-}
-
-function createFirstRow() {
-  let htmlStr = `<tr>`;
+function makeCalendar() {
+  let calendar = document.querySelector('.calendar');
+  dateObj.htmlStr += `<tr>`;
   for (i = 0; i < dateObj.emptyDays; i++) {
-    htmlStr += `<td class="col"></td>`
+    dateObj.htmlStr += `<td class="col"></td>`
   }
   for (i = 1; i < 8 - dateObj.emptyDays; i++) {
-    htmlStr += `<td class="col">${i}</td>`
+    dateObj.htmlStr += `<td class="col">${i}</td>`
   }
-  htmlStr += `</tr>`;
+  dateObj.htmlStr += `</tr>`;
   dateObj.monStart = 8 - dateObj.emptyDays;
-  buildRest(htmlStr);
-}
-
-function buildRest(htmlStr) {
-  let tableBody = document.querySelector('.table__body');
   for (i = 0; i < dateObj.rows; i++) {
-    htmlStr += `<tr>`
+    dateObj.htmlStr += `<tr>`
     for (j = dateObj.monStart; j < dateObj.monStart+7; j++) {
       if (j > dateObj.numDays) break;
       if (j === dateObj.today)
-      htmlStr += `<td class="today col">${j}</td>`;
+      dateObj.htmlStr += `<td class="today col">${j}</td>`;
       else
-      htmlStr += `<td class="col">${j}</td>`;
+      dateObj.htmlStr += `<td class="col">${j}</td>`;
     }
-    htmlStr += '</tr>';
+    dateObj.htmlStr += '</tr>';
     dateObj.monStart = j;
   }
-  tableBody.innerHTML = htmlStr;
+  dateObj.htmlStr += `</tbody></table>`
+  calendar.innerHTML = dateObj.htmlStr;
 }
 
 window.onload = function() {
   let now = new Date();
-  let month = now.getMonth();
+  let curr_month = now.getMonth();
   let year = now.getFullYear();
-  let firstDay = new Date(year, month, 1).toString().split(' ')[0];
+  let firstDay = new Date(year, curr_month, 1).toString().split(' ')[0];
   let today = Math.abs(now.toString().split(' ')[2].split('')[1]);
     dateObj.now = now;
-    dateObj.month = calendar_months[month];
+    dateObj.curr_month = calendar_months[curr_month];
     dateObj.year = year;
     dateObj.firstDay = firstDay;
-    dateObj.numDays = months_days[month];
+    dateObj.numDays = months_days[curr_month];
     dateObj.emptyDays = days.indexOf(firstDay);
     dateObj.rows = Math.ceil((dateObj.numDays + dateObj.emptyDays) / 7);
     dateObj.today = today;
-  makeCalendarHeader();
+    dateObj.htmlStr = `<table><caption class="calendar__head">${dateObj.curr_month} ${dateObj.year}</caption><thead><tr><th class="col">M</th><th class="col">T</th><th class="col">W</th><th class="col">T</th><th class="col">F</th><th class="col">S</th><th class="col">S</th></tr></thead><tbody class="table__body">`
+  makeCalendar();
 }
